2d924af5ae865d3a3f71986ae53e63577bc463f9
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-04-17 13:29:17 +0300

    add CPS & Main.java; task 3

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,8 @@
 public class CreditPaymentService {
-    public long calculate(double percent, double result, int amount) {
-        int payment = (int) (((percent / (result - 1)) + percent) * amount);
-        return payment;
+    public int calculate(int amount, int months, double rate) {
+        double nrate = rate / 12 / 100;
+        double powed = Math.pow(1 + nrate, months);
+        double k = nrate * powed / (powed - 1);
+        return (int) Math.round(k * amount);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double result;
-        double payment;
-        int amount = 1_000_000;
-        double percent = 9.99 / 100 / 12;
-        result = Math.pow(1 + percent, 12);
-        payment = service.calculate(percent, result, amount);
-        System.out.println(payment);
-        result = Math.pow(1 + percent, 24);
-        payment = service.calculate(percent, result, amount);
-        System.out.println(payment);
-        result = Math.pow(1 + percent, 36);
-        payment = service.calculate(percent, result, amount);
-        System.out.println(payment);
+        System.out.println("1 year: " + service.calculate(1_000_000, 12, 9.99));
+        System.out.println("2 years: " + service.calculate(1_000_000, 24, 9.99));
+        System.out.println("3 years: " + service.calculate(1_000_000, 36, 9.99));
     }
 }
